1d5a9c2aa96d5512f5915d1facfe0932ec184571
Cristian Abramchuk <abramchukcristian@gmail.com>
2019-11-27 20:20:54 -0300

    login validation

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -22,7 +22,8 @@ import {server} from "variables/constants";
 const useStyles = makeStyles(styles);
 
 export default function LoginPage() {  
-  const login = () => {
+  const login = (e) => {
+    e.preventDefault();
     fetch(server+`user/login`,{ 
       method:"POST",
       body: JSON.stringify({ 
@@ -57,60 +58,62 @@ export default function LoginPage() {
           backgroundPosition: "top center"
         }}
       >
-        <div className={classes.container}>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={3}>
-              <Card>
-                <form className={classes.form}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                    <h4>Login</h4>
-                  </CardHeader>
-                  <CardBody>
-                    <CustomInput
-                      labelText="Email"
-                      id="email"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "email",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Email className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        )
-                      }}
-                    />
-                    <CustomInput
-                      labelText="Senha"
-                      id="password"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "password",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Icon className={classes.inputIconsColor}>
-                              lock_outline
-                            </Icon>
-                          </InputAdornment>
-                        ),
-                        autoComplete: "off"
-                      }}
+        <form onSubmit={login}>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={3}>
+                <Card>
+                  <form className={classes.form}>
+                    <CardHeader color="primary" className={classes.cardHeader}>
+                      <h4>Login</h4>
+                    </CardHeader>
+                    <CardBody>
+                      <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "email",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Email className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Senha"
+                        id="password"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "password",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Icon className={classes.inputIconsColor}>
+                                lock_outline
+                              </Icon>
+                            </InputAdornment>
+                          ),
+                          autoComplete: "off"
+                        }}
 
-                    />       
-                    <span>
-                      <p id="msg" style={{color:"red"}}></p>
-                    </span>           
-                    <Button block color="primary" size="lg" onClick={() => login()}>Entrar</Button>
-                    <Button block simple color="primary" size="lg" href={"/register"}>Cadastre-se</Button>
-                  </CardBody>
-                </form>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
+                      />       
+                      <span>
+                        <p id="msg" style={{color:"red"}}></p>
+                      </span>           
+                      <Button block color="primary" size="lg" type="submit">Entrar</Button>
+                      <Button block simple color="primary" size="lg" href={"/register"}>Cadastre-se</Button>
+                    </CardBody>
+                  </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </form>
       </div>
     </div>
   );
